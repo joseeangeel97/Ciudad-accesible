6919c0d2b713534398815660d7334fd4a5914f43
Sara López Ortega <125909004+SaritaPopez@users.noreply.github.com>
2023-06-01 17:11:16 +0200

    Update server.js
    en el último middleware habia una cosa que no tenia que estar green.chalk

--- server.js
+++ server.js
@@ -69,7 +69,5 @@ app.use((req, res) => {
 
 //Middleware escucha peticiones al puerto 8000
 app.listen(process.env.PORT, () => {
-  console.log(
-    chalk.green(`Server listening at http://localhost:${process.env.PORT}`)
-  );
+  console.log(`Server listening at http://localhost:${process.env.PORT}`)
 });
