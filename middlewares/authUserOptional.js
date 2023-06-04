ebf2942022c0a52564fe53ab903e65422bb6cf81
Sara López Ortega <125909004+SaritaPopez@users.noreply.github.com>
2023-06-04 12:39:42 +0200

    Update authUserOptional.js
    añadir comentario explicación linea 9

--- middlewares/authUserOptional.js
+++ middlewares/authUserOptional.js
@@ -4,15 +4,17 @@ const { generateError } = require('../helpers');
 const authUserOptional = async (req, res, next) => {
   try {
     const { authorization } = req.headers;
-
+    
+      //Si existe token se crea user en el objeto request. En el caso que recibamos un valor "null" evitamos entrar en el if.
     if (authorization && authorization !== 'null') {
+      
       // Aquí almacenamos el token desencriptado.
       let tokenInfo;
 
       try {
         tokenInfo = jwt.verify(authorization, process.env.SECRET);
       } catch {
-        generateError('Token inválido', 401);
+        generateError('El Token inválido', 401);
       }
 
       req.user = tokenInfo;
