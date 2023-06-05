8439cf9283eeabcb68648b9b868981940bbabb20
Javier Romero Garcia <125921160+jaroga19@users.noreply.github.com>
2023-06-05 20:28:52 +0200

    Update getUser.js
    Actualizacion GetUser

--- controllers/users/getUser.js
+++ controllers/users/getUser.js
@@ -1,8 +1,19 @@
 const selectUserByIdQuery = require('../../db/queries/users/selectUserByIdQuery');
 
+const { generateError } = require('../../helpers');
+
 const getUser = async (req, res, next) => {
   try {
-    const user = await selectUserByIdQuery(req.userid);
+    const { userId } = req.params;
+
+    const user = await selectUserByIdQuery(userId);
+
+    if (!user) {
+      generateError('Usuario no encontrado', 404);
+    }
+
+    // Eliminamos el email ya que es privado y no se debe saber.
+    delete user.email;
 
     res.send({
       status: 'ok',
